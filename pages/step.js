a66d8c722f70489192a9e3aa724a4d87118e6840
Cooper Saye <coopersaye@gmail.com>
2022-01-14 12:28:47 -0800

    Draw canvas.

--- pages/step.js
+++ pages/step.js
@@ -5,19 +5,40 @@ let ctx;
 
 const gridWidth = 33;
 const gridHeight = 33;
-const gridSize = 16;
-const width = gridWidth * gridSize;
-const height = gridHeight * gridSize;
+const grid = 16;
+const width = gridWidth * grid;
+const height = gridHeight * grid;
+
+const blockX = 16;
+const blockY = 16;
+
+const goalX = 32;
+const goalY = 16;
+
+const steps = 16;
 
 import styles from '../styles/pages/Step.module.css';
 
 export default function Step() {
   const canvasRef = useRef();
 
+  // draws canvas
+  function draw() {
+    // clear canvas
+    ctx.clearRect(0, 0, width, height);
+    // draw player
+    ctx.fillStyle = 'red';
+    ctx.fillRect(blockX * grid, blockY * grid, grid, grid);
+    // draw goal
+    ctx.fillStyle = 'gold';
+    ctx.fillRect(goalX * grid, goalY * grid, grid, grid);
+  }
+
   // get canvas context on start
   useEffect(() => {
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
+    draw();
   }, []);
 
   return (
