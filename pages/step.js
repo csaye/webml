7d7a3206fd128294110129d8a9d39e61da98b1e5
Cooper Saye <coopersaye@gmail.com>
2022-01-14 12:22:14 -0800

    Set up step canvas.

--- pages/step.js
+++ pages/step.js
@@ -1,8 +1,24 @@
-import styles from '../styles/pages/Step.module.css';
+import { useEffect, useRef } from 'react';
+
+let canvas;
+let ctx;
 
 export default function Step() {
+  const canvasRef = useRef();
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
+      <canvas
+        ref={canvasRef}
+        width="256"
+        height="256"
+      />
     </div>
   );
 }
