0bcfef030371675e2c851ba455b55505d1238c78
Cooper Saye <coopersaye@gmail.com>
2022-01-18 23:27:34 -0800

    Improve index page styling.

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
+      <div className={styles.center}>
+        <h1>WebML</h1>
+      </div>
     </div>
   );
 }
